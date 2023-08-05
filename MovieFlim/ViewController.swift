d326f4a20592aaecafe82ad68e94fdadede4c108
DO VAN HIEP <hiepzxc16@gmail.com>
2023-08-05 21:49:06 +0700

    Initial Commit

---
+++ MovieFlim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieFlim
+//
+//  Created by MACBOOK on 05/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
